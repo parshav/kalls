47e5e1f9f460048b43dad80629ebefbf75162d4f
Parshav <parshav@live.com>
2018-09-28 14:04:04 -0500

    Experimental params

--- networking/src/main/java/com/pv/networking/DSLBuilder.kt
+++ networking/src/main/java/com/pv/networking/DSLBuilder.kt
@@ -1,47 +1,58 @@
 package com.pv.networking
 
-import arrow.core.some
 import com.beust.klaxon.Klaxon
 import com.github.kittinunf.fuel.core.FuelManager
 import com.github.kittinunf.fuel.httpGet
-import com.google.gson.Gson
-import java.lang.reflect.Type
+import kotlin.reflect.KClass
 
 typealias ApiKey = String
 
 class Kalls(baseUrl: String) {
 
+    companion object {
+        const val self = "kalls kalls"
+    }
+
     val apis = mutableMapOf<Any, String>()
     val sss = mutableMapOf<String, Any>()
+    val params = mutableMapOf<String, String>()
+    val ss = mutableSetOf<KClass<Any>>()
 
     init {
         FuelManager.instance.basePath = baseUrl
     }
 
-    operator fun<T> String.invoke(block: Api<T>.() -> (Unit)): Api<T> {
+    operator fun <T> String.invoke(block: Api<T>.() -> (Unit)): Api<T> {
         val a = Api<T>(this)
         a.block()
         apis[a] = this
         return a
     }
 
-    fun<T : KallRequest> kallRequest(block: Api<T>.() -> Unit): Api<T> {
+    /* inline operator fun<reified T> String.invoke(block: Api2.() -> (Unit)): Api2 {
+         val a = Api2(this)
+         val k = Klaxon().parse<T>(" ")
+         sss[this] = a
+         return a
+     }
+ */
+    fun <T : KallRequest> kallRequest(block: Api<T>.() -> Unit): Api<T> {
         val a = Api<T>("")
         a.block()
         apis[a] = " "
         return a
     }
 
-    infix fun<T> Api<T>.referAs(string: String) {
+    infix fun <T> Api<T>.referAs(string: String) {
         sss[string] = this
     }
 
-    infix fun String.callback(string: String) {
+    fun String.callback(string: String) {
 
     }
 
 
-    fun<T> call(block: (String) -> Unit) {
+    fun <T> call(block: (String) -> Unit) {
         val api = Api<T>("")
         apis[api]?.let {
             block.invoke(it)
@@ -50,19 +61,17 @@ class Kalls(baseUrl: String) {
         }
     }
 
-    fun call2(callback: (Pair<Boolean, T>) -> (Unit)) {
-
+/*    inline fun <reified T> call2(callback: (Pair<Boolean, T>) -> (Unit)) {
         ext.httpGet().responseString { _, _, r ->
             r.fold({
-                //                val type: Type = callback::class.java
-                val k = Klaxon().parse<Type>("")
+                val k = Klaxon().parse<T>(it)
                 k?.let {
                 }
             }, {
 
             })
         }
-    }
+    }*/
 
     fun makeKall() {
 
@@ -79,16 +88,34 @@ infix fun Kalls.kall(something: String) {
 }
 
 
+class Api<T>(
+        val ext: String
+) {
 
-class Api<T>(private val ext: String) {
+
+    inline fun <reified G> call(callback: (Pair<Boolean, G>) -> (Unit)) {
+
+        ext.httpGet().responseString { _, _, r ->
+            r.fold({
+                val k = Klaxon().parse<G>(it)
+                k?.let {
+                }
+            }, {
+
+            })
+        }
+    }
+}
+
+class Api2(val ext: String) {
 
     lateinit var handleError: Error
 
-    fun call(callback: (Pair<Boolean, T>) -> (Unit)) {
+    inline fun <reified T> call(callback: (Pair<Boolean, T>) -> (Unit)) {
+
         ext.httpGet().responseString { _, _, r ->
             r.fold({
-//                val type: Type = callback::class.java
-                val k = Klaxon().parse<Type>("")
+                val k = Klaxon().parse<T>(it)
                 k?.let {
                 }
             }, {
--- networking/src/main/java/com/pv/networking/Networking.kt
+++ networking/src/main/java/com/pv/networking/Networking.kt
@@ -1,6 +1,7 @@
 package com.pv.networking
 
 import android.util.Log
+import com.pv.networking.Kalls.Companion.self
 
 
 typealias launchFunCall = (LaunchReturn) -> Unit
@@ -11,17 +12,27 @@ object Networking {
 
         "launch/next/n" <LaunchReturn> {
 
-        }
+            params["n"] = "10"
+
+            "" <Launch5> {
+
+                params["n"] = "5"
+
+            } referAs "five launches"
+
+        } referAs self
 
         "missions" <MissionReturn> {
 
-            handleError = LaunchError
+//            handleError = LaunchError
 
         } referAs "main missions"
 
         val next5 = "launch/next/5" <LaunchReturn> {
 
         }
+
+        fun a() = next5
 
         next5 referAs "LSD"
     }
@@ -34,15 +45,24 @@ object Networking {
 
         api kall "LSD"
 
+      /*  launchCal {
+
+        }
+        */
+        "hello"
     }
 
     fun launchCall(callBack: (String) -> Unit) = api.call<LaunchReturn> (callBack)
+
+//    fun launchCal(callBack: (Pair<Boolean, LaunchReturn>) -> Unit) = api.call2(callBack)
+    fun authCall() = api.sss["LSD"]
 
 }
 
 data class LaunchReturn(val string: String)
 data class MissionReturn(val string: String)
 
+typealias Launch5 = LaunchReturn
 data class LaunchRequest(val string: String): KallRequest()
 
 object LaunchError : Error()
